5fb8dde29d37907ff6db7170c7c73198f3937b14
Arno Leruste <recroX91@hotmail.com>
2024-11-02 18:47:47 +0100

    refacto

--- PHILOBMBAPI/Controllers/InvoiceController.cs
+++ PHILOBMBAPI/Controllers/InvoiceController.cs
@@ -4,14 +4,14 @@ using PHILOBMCore.Models;
 
 namespace PHILOBMBAPI.Controllers;
 
-    public class InvoiceController : BaseController<Invoice,IInvoiceService>
-    {
-        private readonly IInvoiceService _invoiceService;
+public class InvoiceController : BaseController<Invoice,IInvoiceService>
+{
+    private readonly IInvoiceService _invoiceService;
 
-        public InvoiceController(IInvoiceService invoiceService) : base(invoiceService) 
-        {
-            _invoiceService = invoiceService;
-        }
+    public InvoiceController(IInvoiceService invoiceService) : base(invoiceService) 
+    {
+        _invoiceService = invoiceService;
+    }
 
     [HttpGet("pdf/{invoiceId}")]
     public async Task<ActionResult<byte[]>> CreerPDFAsync(int invoiceId)
@@ -21,29 +21,12 @@ namespace PHILOBMBAPI.Controllers;
         return File(fileContentPdf.FileContents, fileContentPdf.ContentType, fileContentPdf.FileDownloadName);
     }
 
-    //[HttpGet("pdf/{invoiceId}")]
-    //public async Task<IActionResult> CreerPDFAsync(int invoiceId)
-    //{
-    //    var fileContentPdf = await _invoiceService.CreerPDFAsync(invoiceId);
-    //    if (fileContentPdf == null) return NotFound();
-
-    //    return Ok(fileContentPdf); // Retournez un objet JSON
-    //}
-
     [HttpGet("mock")]
-        public async Task<ActionResult<List<Invoice>>> LoadMockInvoices()
-        {
-            var invoices =  _invoiceService.LoadMockInvoices();
-            if (invoices == null) return this.NotFound();
-            return this.Ok(invoices);
-        }
-
-
-    //[HttpGet("{invoiceId}")]
-    //public async Task<ActionResult<IEnumerable<Invoice>>> GetInvoicesByClientIdAsync(int invoiceId)
-    //{
-    //    var invoice = await _invoiceService.GetInvoicesByClientIdAsync(invoiceId);
-    //    return HandleResult(invoice, "Facture non trouvée.");
-    //}
+    public async Task<ActionResult<List<Invoice>>> LoadMockInvoices()
+    {
+        var invoices =  _invoiceService.LoadMockInvoices();
+        if (invoices == null) return this.NotFound();
+        return this.Ok(invoices);
+    }
 }
 
