f2a8f9df4a74fc25e9e9f1a3cc598126870d2b94
Arno Leruste <recroX91@hotmail.com>
2024-11-02 21:27:13 +0100

    change RacinePath

--- Configuration/Constants/Constants.cs
+++ Configuration/Constants/Constants.cs
@@ -8,9 +8,12 @@ public static class Constants
     public const string DBName = "philoBM.db";
     public const int MaxBackupCount = 100;
     public const bool ShowMessageBoxes = false;
+    public const string MimeTypePDF = "application/pdf";
     public static readonly string DownloadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
-    public static readonly string RacinePath = Path.Combine("C:", "PhiloBM");
     public static readonly string DbPath = Path.Combine(RacinePath, DBName); // Chemin complet vers la DB
 
-    public const string MimeTypePDF = "application/pdf";
+
+
+    //public static readonly string RacinePath = Path.Combine("C:", "PhiloBM");
+    public static string RacinePath => Path.Combine(Path.GetPathRoot(AppContext.BaseDirectory) ?? "C:\\", "PhiloBM");
 }
